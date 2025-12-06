bd04e2acb5a34abbdf0e5f22f02d51285a488918
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-12-07 00:36:14 +0100

    Use tt adjusted eval as condition in nmp (#199)
    ```
    Elo   | 1.96 +- 1.38 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.99 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 80348 W: 20695 L: 20242 D: 39411
    Penta | [885, 9543, 18904, 9918, 924]
    ```
    https://furybench.com/test/4005/
    
    Bench 2254013

--- src/search.cpp
+++ src/search.cpp
@@ -124,6 +124,13 @@ int Search::pvs(int alpha, int beta, int depth, const int ply, Board &board, boo
         }
     }
 
+    int ttAdjustedEval = staticEval;
+
+    if (!isSingularSearch && hashedMove != Move::NULL_MOVE && !inCheck && ((hashedType == UPPER && hashedScore <= staticEval) || (hashedType == LOWER && hashedScore >= staticEval) ||  hashedType == EXACT)) {
+        ttAdjustedEval = hashedScore;
+    }
+
+
     // Reverse Futility Pruning
     // If we subtract a margin from our static evaluation, and it is still far
     // above beta, we can assume that the node will fail high (beta cutoff) and prune it
@@ -143,7 +150,7 @@ int Search::pvs(int alpha, int beta, int depth, const int ply, Board &board, boo
     // If our position is excellent we pass a move to our opponent.
     // If the search returns a score above beta we can cut that off.
     // For more information please look at docs/nmp.md
-    if (!isSingularSearch && !pvNode && depth > 3 && !inCheck && staticEval >= beta) {
+    if (!isSingularSearch && !pvNode && depth > 3 && !inCheck && ttAdjustedEval >= beta) {
         const int nmpDepthReduction = nmpBase + depth / nmpDiv;
         stack[ply].previousMovedPiece = PieceType::NONE;
         stack[ply].previousMove = Move::NULL_MOVE;
